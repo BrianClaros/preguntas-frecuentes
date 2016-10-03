cc1908e56dc976e6cd7a1484237124e8426e53c2
BrianClaros <braian1452@hotmail.com>
2016-10-02 21:01:10 -0300

    Carga manual -2

--- admin/excel/carga/manual/php/cargar.php
+++ admin/excel/carga/manual/php/cargar.php
@@ -8,7 +8,7 @@ $rows = $preguntar->num_rows;
 if($rows!=0){
 	header("Location: ../../../index.php?print=false");
 }else{
-$insert= $conn->query("INSERT INTO preguntas VALUES(NULL, '$pregunta', '$respuesta','0','0')");
+$insert= $conn->query("INSERT INTO preguntas VALUES(NULL, '¿$pregunta?', '$respuesta','0','0')");
 }
 
 
